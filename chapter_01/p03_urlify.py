cc977117b694d5c85e14a52b9dfbabcc0fe91bea
Efe Cini <efecini@gmail.com>
2022-01-07 16:16:11 +0300

    change p03_urlify.py

---
+++ chapter_01/p03_urlify.py
@@ -0,0 +1,55 @@
+# O(N)
+import unittest
+
+def urlify_pythonic_2(string, length):
+    s2 = []
+    #Replace spaces starting from length backwards
+    for c in range(length-1, -1, -1):
+        s2.insert(0, '%20') if string[c] == ' ' else s2.insert(0, string[c])
+    # Convert to string
+    return ''.join(s2)
+
+def urlify_algo(string, length):
+    """replace spaces with %20 and removes trailing spaces"""
+    # convert to list because Python strings are immutable
+    char_list = list(string)
+    new_index = len(char_list)
+
+    for i in reversed(range(length)):
+        if char_list[i] == " ":
+            # Replace spaces
+            char_list[new_index - 3 : new_index] = "%20"
+            new_index -= 3
+        else:
+            # Move characters
+            char_list[new_index - 1] = char_list[i]
+            new_index -= 1
+    # convert back to string
+    return "".join(char_list[new_index:])
+
+
+def urlify_pythonic(text, length):
+    """solution using standard library"""
+    return text[:length].replace(" ", "%20")
+
+
+class Test(unittest.TestCase):
+    """Test Cases"""
+
+    test_cases = {
+        ("much ado about nothing      ", 22): "much%20ado%20about%20nothing",
+        ("Mr John Smith       ", 13): "Mr%20John%20Smith",
+        (" a b    ", 4): "%20a%20b",
+        (" a b       ", 5): "%20a%20b%20",
+    }
+    testable_functions = [urlify_algo, urlify_pythonic, urlify_pythonic_2]
+
+    def test_urlify(self):
+        for urlify in self.testable_functions:
+            for args, expected in self.test_cases.items():
+                actual = urlify(*args)
+                assert actual == expected, f"Failed {urlify.__name__} for: {[*args]}"
+
+
+if __name__ == "__main__":
+    unittest.main()
